2c9d4a94f24a9c8b57e3ddb3440c5f8a720bc7d5
Moein Moeinnia <moeinnia@protonmail.com>
2023-08-16 22:24:14 +0430

    main.ts for auth microservice

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,19 +2,38 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-
-  app.useLogger(app.get(Logger , {strict : false}))
   
   const configService = app.get(ConfigService)
   
+  app.enableCors();
+
+  //Enabling versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.useLogger(app.get(Logger , {strict : false}))
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   
+  //Enabling Prisma
+  //const prismaService = app.get(PrismaService);
+  //await prismaService.enableShutdownHooks(app);
 
   const PORT = configService.get("HTTP_PORT");
   console.log(PORT);
-  
 
   await app.listen(PORT);
 }
