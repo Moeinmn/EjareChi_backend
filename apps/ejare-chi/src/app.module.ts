1a90a8ffbb5f684bcd2b40f62c96388ff4288f8e
Moein Moeinnia <moeinnia@protonmail.com>
2023-08-16 22:29:32 +0430

    common module files added

--- apps/ejare-chi/src/app.module.ts
+++ apps/ejare-chi/src/app.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './v1/auth/auth.module';
 import { ProductsModule } from './v1/products/products.module';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
-import { ElasticModule } from './infrastructure/elasticsearch/elasticsearch.module';
+import { ElasticModule } from '../../../libs/common/src/elasticsearch/elasticsearch.module';
 import { UserInterceptor } from './v1/interceptors/user.interceptor';
 // import { SearchModule } from './infrastructure/elasticsearch/elasticsearch.module';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
