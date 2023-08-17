6c1cf9f73c0df8abd4f8e58685b2db08802c7157
Moein Moeinnia <moeinnia@protonmail.com>
2023-08-17 20:05:18 +0430

    some dependancy injection missings fixed

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,17 +2,29 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { PrismaModule, RedisModule } from '@app/common';
+import { CustomLoggerModule, PrismaModule, RedisModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
 
 
 
 
 @Module({
   imports: [PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // validationSchema: Joi.object({
+      //   MONGODB_URI: Joi.string().required(),
+      //   JWT_SECRET: Joi.string().required(),
+      //   JWT_EXPIRATION: Joi.string().required(),
+      //   HTTP_PORT: Joi.number().required(),
+      //   TCP_PORT: Joi.number().required(),
+      // }),
+    }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '24h' }, // Optional: Token expiration time
     }),
+    CustomLoggerModule,
     RedisModule
   ],
   providers: [AuthService],
--- apps/rental/src/rental.module.ts
+++ apps/rental/src/rental.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { RentalController } from './rental.controller';
 import { RentalService } from './rental.service';
-import { PrismaModule } from '@app/common';
+import { CustomLoggerModule, PrismaModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // validationSchema: Joi.object({
+      //   MONGODB_URI: Joi.string().required(),
+      //   JWT_SECRET: Joi.string().required(),
+      //   JWT_EXPIRATION: Joi.string().required(),
+      //   HTTP_PORT: Joi.number().required(),
+      //   TCP_PORT: Joi.number().required(),
+      // }),
+    }),
+    CustomLoggerModule,
+  ],
   controllers: [RentalController],
   providers: [RentalService],
 })
